cb701951fb1a80f09f8d1a2f45b0cb972a4ff12d
connected-psobocinski <psobocinski@connected.io>
2022-07-19 16:33:23 -0400

    Shorten a given URL and store it in the DB

--- app/url_shortener.rb
+++ app/url_shortener.rb
@@ -1,2 +1,12 @@
+require 'daybreak'
+
 class URLShortener
+    def self.shorten(long_url)
+        short_url = 'https://conn.io/' + rand(1000..99999).to_s(36)
+        db = Daybreak::DB.new './data/urls.db'
+        db.set!(short_url, long_url)
+        db.close
+
+        short_url
+    end
 end
--- spec/url_shortener_spec.rb
+++ spec/url_shortener_spec.rb
@@ -1,4 +1,27 @@
 require './app/url_shortener'
 
 RSpec.describe URLShortener do
+    before(:all) do
+        @db = Daybreak::DB.new './data/urls.db'
+    end
+
+    describe '.shorten' do
+        it 'shortens the url' do
+            short_url = URLShortener.shorten('https://www.yahoo.com')
+
+            expect(short_url.size).to be < 'https://www.yahoo.com'.size
+        end
+
+        it 'writes shortened url the db' do
+            short_url = URLShortener.shorten('https://www.google.com')
+
+            @db.load
+            retrieved_url = @db[short_url]
+            expect(retrieved_url).to eq 'https://www.google.com'
+        end
+    end
+
+    after(:all) do
+        @db.close
+    end
 end
