ee31d770c9c00f8523526d80f42347f6bb02b801
Paul Sobocinski <psobocinski@thoughtworks.com>
2024-06-12 00:44:32 -0400

    allow for the url generator to be swapped out

--- app/url_shortener.rb
+++ app/url_shortener.rb
@@ -1,21 +1,21 @@
 require 'daybreak'
 
 class URLShortener
-    def self.shorten(full_length_url, db)
-      short_url = URLGenerator.generate
-      db.set!(short_url, full_length_url)
-      short_url
-    end
-  
-    def self.retrieve(short_url, db)
-      raise "URL not found: #{short_url}" unless db.keys.include?(short_url)
-  
-      db[short_url]
-    end
+  def self.shorten(full_length_url, db, url_generator = DefaultURLGenerator.new)
+    short_url = url_generator.generate
+    db.set!(short_url, full_length_url)
+    short_url
   end
-  
-  class URLGenerator
-    def self.generate
-      'https://conn.io/' + rand(1000..99999).to_s(36)
-    end
+
+  def self.retrieve(short_url, db)
+    raise "URL not found: #{short_url}" unless db.keys.include?(short_url)
+
+    db[short_url]
   end
+end
+
+class DefaultURLGenerator
+  def generate
+    'https://conn.io/' + rand(1000..99999).to_s(36)
+  end
+end
