e4a96f3c9ec9e27328baaccc33c7f76c2cffb5e6
Paul Sobocinski <psobocinski@thoughtworks.com>
2024-06-12 00:40:12 -0400

    extract url generator

--- app/url_shortener.rb
+++ app/url_shortener.rb
@@ -2,14 +2,20 @@ require 'daybreak'
 
 class URLShortener
     def self.shorten(full_length_url, db)
-        short_url = 'https://conn.io/' + rand(1000..99999).to_s(36)
-        db.set!(short_url, full_length_url)
-        short_url
+      short_url = URLGenerator.generate
+      db.set!(short_url, full_length_url)
+      short_url
     end
-
+  
     def self.retrieve(short_url, db)
-        raise "URL not found: #{short_url}" unless db.keys.include?(short_url)
-
-        db[short_url]
+      raise "URL not found: #{short_url}" unless db.keys.include?(short_url)
+  
+      db[short_url]
     end
-end
+  end
+  
+  class URLGenerator
+    def self.generate
+      'https://conn.io/' + rand(1000..99999).to_s(36)
+    end
+  end
