294fcc0505a4a182611dda910985157413bccfd6
Paul Sobocinski <psobocinski@thoughtworks.com>
2024-06-12 00:28:04 -0400

    remove unneeded db.close calls

--- app/url_shortener.rb
+++ app/url_shortener.rb
@@ -4,19 +4,15 @@ class URLShortener
     def self.shorten(full_length_url, db)
         short_url = 'https://conn.io/' + rand(1000..99999).to_s(36)
         db.set!(short_url, full_length_url)
-
         short_url
     end
 
     def self.retrieve(short_url, db)
         unless db.keys.include?(short_url)
-            db.close
             raise "URL not found: #{short_url}"
         end
 
         full_length_url = db[short_url]
-        db.close
-
         full_length_url
     end
 end
