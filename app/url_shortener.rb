2b2554c3b8dbdc9cde937a25a95394f38ff9e3c5
Paul Sobocinski <psobocinski@thoughtworks.com>
2024-06-12 10:56:47 -0400

    tmp commit

--- app/url_shortener.rb
+++ app/url_shortener.rb
@@ -1,18 +1,15 @@
 require 'daybreak'
 
 class URLShortener
-    def self.shorten(full_length_url)
+    def self.shorten(full_length_url, db = Daybreak::DB.new('./data/urls.db'))
         short_url = 'https://conn.io/' + rand(1000..99999).to_s(36)
-        db = Daybreak::DB.new './data/urls.db'
         db.set!(short_url, full_length_url)
         db.close
 
         short_url
     end
 
-    def self.retrieve(short_url)
-        db = Daybreak::DB.new './data/urls.db'
-
+    def self.retrieve(short_url, db = Daybreak::DB.new('./data/urls.db'))
         unless db.keys.include?(short_url)
             db.close
             raise "URL not found: #{short_url}"
